2395339b23661a73a7e26d86290a5ec1c550b3ab
qod3r <primculov@protonmail.ch>
2023-09-12 01:16:51 +0700

    ui and warn logic working

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from pprint import pprint
 
 from pypdf import PdfMerger
 from PyQt5 import uic
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QApplication, QFileDialog, QTreeWidgetItem, QWidget
 
 basedir = os.path.dirname(__file__)
+
+# порядок слияния
+# order1 = ["паспорт", "разметка", "лг"]
+# order2 = [*[f"п{i}" for i in range(1, 9)], *[f"о{i}" for i in range(1, 9)]]
 
 
 class MainWidget(QWidget):
@@ -26,50 +31,154 @@ class MainWidget(QWidget):
             self.load_streets(self.path, self.treeWidget)
 
     def load_streets(self, path: Path, tree: QTreeWidgetItem):
-        order1 = ["паспорт", "разметка", "лг"]
-        order2 = [*[f"п{i}" for i in range(1, 9)], *[f"о{i}" for i in range(1, 9)]]
-        
-        self.street_dirs = []
+        self.streets = []
         anchor_dir_name = None
         anchor_dir_idx = None
-        anchor_dir_tree = None
+        anchor_dir_root = None
         for i, element in enumerate(path.iterdir()):
             if not element.is_dir():
                 continue
             # начальный случай
+            # TODO: лишний
             if i == 0:
+                street = Street()
+                road = street.add_road(element)
+                self.streets.append(street)
+
                 anchor_dir_name = element.name
                 anchor_dir_idx = i
-                anchor_dir_tree = QTreeWidgetItem(
+                # добавляем читабельное название улицы в дерево
+                # и делаем его "якорем"
+                anchor_dir_root = QTreeWidgetItem(
                     tree, [folder_to_root_name(element.name)]
                 )
-                QTreeWidgetItem(anchor_dir_tree, [element.name])
-                self.street_dirs.append([element])
+
+                # полное название внутри улицы
+                item = QTreeWidgetItem(anchor_dir_root, [element.name])
+                item.setBackground(0, QColor(road.color()))
+                populate_problems(item, road.status)
+                anchor_dir_root.setBackground(0, QColor(street.color()))
                 continue
             # n-ая часть улицы (очень наивно)
-            if any(
+            elif any(
                 # 💀
                 x in [anchor_dir_name, anchor_dir_name[:-1], anchor_dir_name[:-2]]
                 for x in [element.name[:-2], element.name[:-1]]
             ):
-                QTreeWidgetItem(anchor_dir_tree, [element.name])
-                self.street_dirs[anchor_dir_idx].append(element)
+                road = self.streets[anchor_dir_idx].add_road(element)
+
+                item = QTreeWidgetItem(anchor_dir_root, [element.name])
+                item.setBackground(0, QColor(road.color()))
+                populate_problems(item, road.status)
+                anchor_dir_root.setBackground(0, QColor(street.color()))
             # 1-ая часть улицы
             else:
+                street = Street()
+                road = street.add_road(element)
+                self.streets.append(street)
+
                 anchor_dir_name = element.name
                 anchor_dir_idx += 1
-                anchor_dir_tree = QTreeWidgetItem(
+                anchor_dir_root = QTreeWidgetItem(
                     tree, [folder_to_root_name(element.name)]
                 )
-                QTreeWidgetItem(anchor_dir_tree, [element.name])
-                self.street_dirs.append([element])
-        pprint(self.street_dirs)
 
+                item = QTreeWidgetItem(anchor_dir_root, [element.name])
+                item.setBackground(0, QColor(road.color()))
+                populate_problems(item, road.status)
+                anchor_dir_root.setBackground(0, QColor(street.color()))
+        pprint(self.streets)
+        # pprint(Road(self.street_dirs[0][1], False).status)
+
+    def check_street(self):
+        ...
 
 def folder_to_root_name(s):
     return "".join(
         filter(lambda ch: not ch.isdigit(), " ".join(s.replace("_", " ").split()[-2:]))
     ).strip()
+
+def populate_problems(root, status):
+    for err in status["err"]:
+        item = QTreeWidgetItem(root, [err])
+        item.setBackground(0, QColor(status["color"]))
+    for warn in status["warn"]:
+        item = QTreeWidgetItem(root, [warn])
+        item.setBackground(0, QColor(status["color"]))
+
+class Street:
+    def __init__(self, main_road=None):
+        self.roads = []
+        if main_road is not None:
+            self.roads.append(Road(main_road, True))
+
+    def add_road(self, path: Path):
+        main = False
+        if not self.roads:
+            main = True
+        road = Road(path, is_main=main)
+        self.roads.append(road)
+        return road
+
+    def color(self):
+        # red
+        if any(r.status["color"] == "#BF616A" for r in self.roads):
+            return "#BF616A"
+        # yellow
+        elif any(r.status["color"] == "#EBCB8B" for r in self.roads):
+            return "#EBCB8B"
+        # green
+        else:
+            return "#A3BE8C"
+
+    def __repr__(self):
+        return str(self.roads)
+
+
+class Road:
+    # TODO: настройка префиксов в названиях файлов
+    # TODO: полные названия в ошибках (лг - Линейный график и т.д.)
+
+    # всегда должен быть линейный график и данные прямого+обратного направления
+    required = ["лг", *[f"п{i}" for i in range(1, 9)], *[f"о{i}" for i in range(1, 9)]]
+    # паспорт у первой части
+    conditional = ["паспорт"]
+    # разметка если есть
+    warn = ["разметка"]
+
+    def __init__(self, path: Path, is_main: bool):
+        self.path = path
+        self.is_main = is_main
+        self.pdfs = []
+        for element in self.path.iterdir():
+            if ".pdf" in element.name:
+                self.pdfs.append(element.name)
+
+        s = {"ok": [], "warn": [], "err": [], "color": "#A3BE8C"}
+        for pref in self.warn:
+            # пока что проверяем ошибки в разметке
+            # if not any(pref in x for x in self.pdfs):
+            if any(pref in x for x in self.pdfs):
+                s["warn"].append(f"Проверить {pref}.pdf (заголовок раздельно)")
+                s["color"] = "#EBCB8B"
+        for pref in self.required:
+            if not any(pref in x for x in self.pdfs):
+                s["err"].append(f"Отсутствует {pref}.pdf")
+                s["color"] = "#BF616A"
+        for pref in self.conditional:
+            if (not any(pref in x for x in self.pdfs)) and self.is_main:
+                s["err"].append(f"Отсутствует {pref}.pdf")
+                s["color"] = "#BF616A"
+
+        # оставшиеся
+        s["ok"] = self.pdfs.copy()
+        self.status = s
+
+    def color(self):
+        return self.status["color"]
+
+    def __repr__(self):
+        return f"{'Main' if self.is_main else 'Secondary'} {self.path.name}"
 
 
 if __name__ == "__main__":
